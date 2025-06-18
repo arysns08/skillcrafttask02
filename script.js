caa8884986125350dcb4a7b381518c03531623ae
arysns08 <157990243+arysns08@users.noreply.github.com>
2025-06-18 16:14:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let timer;
+let isRunning = false;
+let startTime;
+let elapsedTime = 0;
+
+const display = document.getElementById('display');
+const startStopButton = document.getElementById('startStop');
+const resetButton = document.getElementById('reset');
+const lapButton = document.getElement
